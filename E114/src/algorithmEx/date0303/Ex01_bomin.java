cf12d67c0ad79bf1d273a5253ee4e7d4db73cef1
bomin1996 <bomin199628@gmail.com>
2023-03-02 19:09:39 +0900

    문제 2

---
+++ E114/src/algorithmEx/date0303/Ex01_bomin.java
@@ -0,0 +1,17 @@
+package algorithmEx.date0303;
+
+public class Ex01_bomin {
+	
+	//url 문제 : https://school.programmers.co.kr/learn/courses/30/lessons/120884?language=java
+	public int solution(int chicken) {
+        int answer = 0;
+        int coupon = 0;
+        while(chicken >= 10) {
+            coupon = chicken / 10;
+            chicken = chicken % 10 + coupon;
+            answer += coupon;
+        }
+        return answer;
+    }
+
+}
