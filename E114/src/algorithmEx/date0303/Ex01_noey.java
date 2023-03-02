5ca1770a858c50a956d0b6d1d8560a9fdb7a7c6e
Noey <usky0604@gmail.com>
2023-03-02 18:54:24 +0900

    chicken noey

---
+++ E114/src/algorithmEx/date0303/Ex01_noey.java
@@ -0,0 +1,15 @@
+package E114.src.algorithmEx.date0303;
+
+public class Ex01_noey {
+	
+	//url 문제 : https://school.programmers.co.kr/learn/courses/30/lessons/120884?language=java
+	public int solution(int chicken) {
+		int coupon = chicken;
+		int service = 0;
+        while(coupon >= 10) {
+        	service += coupon / 10;
+        	coupon = coupon / 10 + (coupon % 10);
+        }
+        return service;
+    }
+}
